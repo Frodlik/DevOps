b2978f91da62b565b0ff342336518431b382a9d9
root <root@MyVM.myguest.virtualbox.org>
2024-10-24 19:11:58 +0300

    Updated FuncA to calculate sum of first n elements

--- funcA.cpp
+++ funcA.cpp
@@ -1,6 +1,13 @@
 #include "funcA.h"
+#include <cmath>
 
-//Placeholder implementation that returns 0
-double FuncA::calculate() {
-	return 0;
+// Function to calculate sum of first n elements in the geometric series
+// Param n: number of elements
+// Param x: value of x in the series
+double FuncA::calculate(int n, double x) {
+	double sum = 0;
+	for(int i = 0; i < n; ++i) {
+		sum += pow(x, i);
+	}
+	return sum;
 }
